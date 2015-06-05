779b30a9c1c797611cbff2327e94681415e8933b
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2015-06-05 18:45:17 -0500

    test

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,36 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="/path/to/parallax.min.js"></script>
+
+//$(document).on("ready", function(){
+//	$("form").submit(function(){
+//	var number1 = parseInt($(".Pnumber1").val());
+//	var number2 = parseInt($(".Pnumber2").val());
+//	var suma = (number1 + number2);
+//  	alert (suma);
+//	});	
+//
+//	$(".p1").on("click",function(){
+//		$(this).toggleClass("verde")
+//	});
+//
+//	$(".p2").on("click",function(){
+//		$(this).addClass("azul")
+//	});
+//});
+
+
